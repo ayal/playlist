7dc76dda31fefd69518dfc4429ebaeba15867c7c
ayal <ayalgelles@gmail.com>
2016-08-23 13:42:57 +0300

    90%

--- client/index.js
+++ client/index.js
@@ -7,21 +7,37 @@ import {songs} from './song-list.json'
 
 import SortableMixin from 'sortablejs/react-sortable-mixin.js'
 
+// mutable data
+var allsongs = songs;
+
 var AllSongs = React.createClass({
   mixins: [SortableMixin],
 
   sortableOptions: {
     ref: "song",
     group: "shared",
-    animation: 1000,
+    animation: 400,
     model: "songs"
   },
 
   getInitialState: function() {
-    return { songs };
+    return { songs: allsongs };
+  },
+
+  addSong: function(ri) {
+    return function() {
+      var song = this.state.songs[ri];
+      playlist.push(song);
+      allsongs = allsongs.filter((x,i)=>(i !== ri));
+      this.setState({songs:allsongs});
+      
+      playlistUpdate();
+    }.bind(this)
   },
 
   render: function() {
+    var that = this;
+    
     return (<div className="all-songs song-list">
       <div className="header">
       <span className="icon-search"></span>
@@ -34,7 +50,7 @@ var AllSongs = React.createClass({
       <div ref="song">{
         this.state.songs.map(function ({artist, title, image}, i) {
           return (
-	    <div ref={i} className="song" key={i} >
+	    <div ref={i} className="song" key={i} onClick={that.addSong(i)}>
 	    <div>
 	    <div className="arrow-left"></div>
 	    <span className="line"><span className="circle"></span></span>
@@ -52,30 +68,69 @@ var AllSongs = React.createClass({
   }
 });
 
+var playlist = [{artist: 'XXYYXX', title: 'About You', image: 'http://a4.mzstatic.com/us/r30/Music62/v4/75/b4/77/75b4779c-75a4-acae-ecb1-23cbcc843df9/cover170x170.jpeg'}];
+
 var ApprovedSongs = React.createClass({
   mixins: [SortableMixin],
   sortableOptions: {
     ref: "song",
     group: "shared",
     animation: 400,
-    model: "songs"
- },
+    model: "songs",
+  },
 
+  handleAdd: function (evt) {
+    if (evt.from.id !== 'playlist' ) {
+      playlist.splice(evt.newIndex, 0, allsongs[evt.oldIndex]);
+      allsongs = allsongs.filter((x,i)=>(i !== evt.oldIndex));
+      playlistUpdate();
+    }
+  },
+  
+  handleRemove: function (evt) {
+    if (playlist.length === 1) {
+      return;
+    }
+    
+    playlist = playlist.filter((x,i)=>(i !== evt.oldIndex));
+    playlistUpdate();
+  },
+
+  handleSort: function (evt) {
+    if (evt.from.id === 'playlist' ) {
+      var old = playlist[evt.oldIndex];
+      playlist = playlist.filter((x,i)=>(i !== evt.oldIndex));
+      playlist.splice(evt.newIndex, 0, old);
+      playlistUpdate();
+    }
+  },
+
+
+  componentDidMount: function() {
+    window.playlistUpdate = function() {
+      this.forceUpdate();
+    }.bind(this);
+  },
+
+  
   getInitialState: function() {
-    return { songs: [{artist: 'XXYYXX', title: 'About You', image: 'http://a4.mzstatic.com/us/r30/Music62/v4/75/b4/77/75b4779c-75a4-acae-ecb1-23cbcc843df9/cover170x170.jpeg'}] };
+    return { songs: playlist };
   },
 
   remove: function(ri) {
     return function() {
-      this.setState({songs:this.state.songs.filter((x,i)=>(i !== ri))});
+      if (playlist.length === 1) {
+	return;
+      }
+      playlist = playlist.filter((x,i)=>(i !== ri));
+      
+      playlistUpdate();
     }.bind(this)
   },
 
   componentWillUpdate: function(np,ns) {
-    // keep list with something
-    if (ns.songs.length === 0) {
-      ns.songs = [{artist: 'Never Gonna Give You Up', title: 'Rick Astley', image: 'http://a1.mzstatic.com/us/r30/Music/v4/f6/fb/02/f6fb0292-6d51-e3c3-deb6-5d7a07ac6efe/cover170x170.jpeg'}];
-    }
+    // don't allow state updates, only data
+    ns.songs = playlist;
   },
   
   render: function() {
@@ -91,12 +146,12 @@ var ApprovedSongs = React.createClass({
       <div><span className="line"><span className="circle"></span></span></div>
       </div>
       
-      <div ref="song">{
+      <div ref="song"  id="playlist" >{
 	this.state.songs.map(function ({artist, title, image}, i) {
 	  var firstlast = ((i === that.state.songs.length - 1) ? 'last' : 'notlast');
 	  
           return (
-	    <div className={"song approved-song " + firstlast} ref={i} key={i}>
+	    <div className={"song approved-song " + firstlast} ref={i} key={i} >
 	    <div>
 	    <span className="line"><span className="circle">{i + 1}</span></span>
 	    <span className={"icon-trash " + (that.state.songs.length === 1 ? 'transp' : '')} onClick={that.remove(i)} ></span>
