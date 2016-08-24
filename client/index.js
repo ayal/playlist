7694922307a063b53c25f3960da07837bbbc93ac
ayal <ayalgelles@gmail.com>
2016-08-24 15:09:10 +0300

    but had to user data.playlist in render for mobx to pick it up?

--- client/index.js
+++ client/index.js
@@ -113,11 +113,7 @@ var ApprovedSongs = observer(React.createClass({
       </div>
       
       <div ref="song"  id="playlist-el" >{
-<<<<<<< HEAD
-	this.props.songs.map(function ({artist, title, image}, i) {
-=======
 	data.playlist.map(function ({artist, title, image}, i) {
->>>>>>> gh-pages
 	  var firstlast = ((i === that.props.songs.length - 1) ? 'last' : 'notlast');
 	  
           return (
